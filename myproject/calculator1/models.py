081bd35ce6dd5997e527fb28840c2950d3b6bde6
abhisheckpo <linkwithabhisheck@gmail.com>
2025-03-27 19:58:35 +0530

    Rectifying Bad State

--- myproject/calculator1/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class SumRecord(models.Model):
-    num1 = models.IntegerField()
-    num2 = models.IntegerField()
-    sum_result = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
--- myproject/calculator1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import calculate_sum
-
-urlpatterns = [
-    path("sum/", calculate_sum, name="calculate_sum"),
-]
